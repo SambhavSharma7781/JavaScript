ff49536edcf26a8ecbf595a5c11f3b380ba9ba78
Sambhav Sharma <sambhav7717@gmail.com>
2025-03-19 18:27:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJs not browser
+
+console.log( // it will run but code readability should be high 
+    3 + 3
+)
+console.log("Sambhav")
+
+let name ="Sambhav" // string
+let age = 20  // Integer 
+let isLoggedIn = true // Boolean
+
+
+// number => range = 2 to the power 53
+// bigInt for larger values 
+// string => " "
+// boolean => true or false
