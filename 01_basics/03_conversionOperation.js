d7034528646a1523f0120e01ea7d7d53cbea7742
Sambhav Sharma <sambhav7717@gmail.com>
2025-03-20 08:23:39 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "77ab"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); 
+
+
+// "77" => number
+// "77ab" => NaN (Not A Number)
+// true => 1
+// null => 0
+// undefined => NaN
+
+let isLoggedIn  = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "sambhav" => true
+
+let value = 77
+
+let valueInString = String(value)
+console.log(valueInString);
+console.log(typeof valueInString);
