ceb49b5c378f6baff38c65e9d86d7992d1710ef6
Sambhav Sharma <sambhav7717@gmail.com>
2025-03-20 08:47:53 +0000

    Operations discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let value = 77
 
 let valueInString = String(value)
-console.log(valueInString);
-console.log(typeof valueInString);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+// *************************** Operations ***************************
+
+let val = 3
+let negVal = -3
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(4/2));
+// console.log(4%2);
+
+let str1 = "Sambhav"
+let str2 = " Sharma"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(2 + 3 + "4");
+
+console.log((3 + 4) * 6 % 4);
+
+
+
+
