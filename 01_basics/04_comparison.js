dfadb96de046ff3be38441fcb7421b9b9693af53
Sambhav Sharma <sambhav7717@gmail.com>
2025-03-20 09:16:18 +0000

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // datatypes must be same while comparing though output will still be ✅
+// console.log("02" > 1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false equality check works differently
+console.log(null >= 0); // true it converts null to 0 then compare it 
+
+
+console.log("2" === 2); // false bcz "===" checks the datatype as well
