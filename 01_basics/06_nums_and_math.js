cb3bc367548e4ad964677e2c9cf6ec90f1e0a362
Sambhav Sharma <sambhav7717@gmail.com>
2025-03-20 13:01:09 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,47 @@
+const score = 100
+// console.log(score);
+
+const val = new Number(100)
+// console.log(val);
+
+// console.log(val.toString().length);
+// console.log(val.toFixed(2));
+
+const otherNumber = 777.555
+// console.log(otherNumber.toPrecision(3));
+
+const hundreads = 1000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+
+// *************************** Maths ***************************
+
+// console.log(Math);
+// console.log(Math.abs(-567));
+// console.log(Math.round(78.476583478));
+// console.log(Math.ceil(78.476583478));
+// console.log(Math.floor(78.476583478));
+// console.log(Math.min(34,5,35,6,1,435,998));
+// console.log(Math.max(34,5,35,6,1,435,998));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) +  1);
+// console.log(Math.floor((Math.random()*10)) +  1);
+
+const min = 400
+const max = 999
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
+
+
+
+
+
+
+
+
