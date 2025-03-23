ea52968d5c635e229a15395606d68be4d3ad240c
Sambhav Sharma <sambhav7717@gmail.com>
2025-03-23 08:47:53 +0000

    scopes in fucntions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,3 +13,38 @@ if (true) {
 // console.log(b);
 // console.log(c);
 
+function one() {
+    const username = "sam"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+    
+}
+
+
+if(true ){
+    const username = "sambhav"
+    if(username === "sambhav"){
+        const website = " google"
+        // console.log(username +  website);
+     }
+}
+
+// *************************** Interesting ***************************
+
+
+console.log(addTwo(7)); // when u store the function in a expression then you cannot access it before its initialisation
+const addTwo = function (num) { 
+    return num + 7
+}
+
+console.log(addOne(7)); //  here it will work fine 
+function addOne(num) {
+    return num + 7 
+}
+
